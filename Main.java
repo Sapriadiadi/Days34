916d46d1adacaf845a450522691f4b9bb4810ec0
Sapriadi <147253388+Sapriadiadi@users.noreply.github.com>
2023-11-11 22:47:07 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+package days34;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        byte ab = 1;
+        System.out.println(ab+" bytes");
+        short as = 2;
+        System.out.println(as+" bytes");
+        int ai = 4;
+        System.out.println(ai+" bytes");
+        long al = 8;
+        System.out.println(al+" bytes");
+        float af = 4;
+        System.out.println(af+" bytes");
+        double ad = 8;
+        System.out.println(ad+" bytes");
+        char ktr = '?';
+        System.out.println(ktr+" karakter");
+        boolean cw = true;
+        System.out.println("Perempuan selalu "+cw);
+        
+        
+    }
+    
+}
